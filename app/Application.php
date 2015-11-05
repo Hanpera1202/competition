cbd50aba13e488d56e30935ce1450c3320239893
nakazato <hiromu.develop@gmail.com>
2015-11-05 21:54:45 +0800

    add to regist apply_date

--- app/Application.php
+++ app/Application.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use DB;
+use Carbon\Carbon;
 
 class Application extends Model
 {
@@ -12,9 +13,12 @@ class Application extends Model
 
     public static function regist($user_id, $competition_id) {
 
+        $now_time = Carbon::now();
+
         $application = new Application;
         $application->user_id = $user_id;
         $application->competition_id = $competition_id;
+        $application->apply_date = $now_time->toDateTimeString();
         $application->save();
 
         return $application->id;
