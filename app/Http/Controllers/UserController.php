75d169d2b45ec8ff943f0bb1c10c5b4a8dfe2262
nakazato <hiromu.develop@gmail.com>
2015-11-08 17:38:11 +0800

    add user/registmail

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,4 +10,10 @@ class UserController extends Controller
         $user_id = User::regist($device_id);
         return response()->json(array("user_id" => $user_id));
     }
+    public function getRegistmail($user_id, $mail_address) {
+        $user = User::find($user_id);
+        $user->mail_address = $mail_address;
+        
+        return response()->json(array("result" => $user->save()));
+    }
 }
